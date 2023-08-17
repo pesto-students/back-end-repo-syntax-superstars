246e0cb1feb2b2ac22cfd293e7afb6c6761a2700
Archana Rangrej <archana@Archanas-MacBook-Pro.local>
2023-08-17 13:59:22 +0530

    feat: remove unused cors options

--- src/server.js
+++ src/server.js
@@ -37,11 +37,11 @@ app.use(function onError(err, req, res, next) {
 });
 
 // Middleware to parse incoming JSON data
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you intend to use
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add any additional headers your app might send
-}));
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you intend to use
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Add any additional headers your app might send
+// }));
 app.use(express.json());
 app.use(cookieParser());
 
